48d95ac9ec93cc52a2954cd9b15be79218a2e456
Haggai-Alawode <haggaialawode26@gmail.com>
2023-03-22 09:04:41 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,30 +1,51 @@
-#include "main.h"
+#include "main.c"
 #include <stdio.h>
 
 /**
- * main - print sum of even Fionacci sequence up to 4,000,000
+ * main - print first 98 Fibonacci numbers without using long long, malloc,
+ * pointers, array/tables, or structures
  * Return: 0
  */
 
 int main(void)
 {
-	int sum_of_evens = 0;
-	int a;
-	int b;
-	int sum = 1;
+	int counter, overflow;
+	unsigned long a = 1;
+	unsigned long b = 1;
+	unsigned long sum = 0;
+	long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
 
-	a = 1;
-	b = 1;
+	printf("1");
 
-	while (b < 4000000)
+	for (counter = 2; counter < 93; counter++) /* will still print */
 	{
 		sum = a + b;
 		a = b;
 		b = sum;
-		if ((sum <= 4000000) && (sum % 2 == 0))
-			sum_of_evens += sum;
+		printf(", %lu", sum);
 	}
-	printf("%d\n", sum_of_evens);
+
+	a_head = a / 1000000000; /* break larger num into 2 parts */
+	a_tail = a % 1000000000;
+	b_head = b / 1000000000;
+	b_tail = b % 1000000000;
+
+	for (; counter < 99; counter++)
+	{
+		overflow = (a_tail + b_tail) / 1000000000;
+		sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
+		sum_head = (a_head + b_head) + overflow;
+
+		printf(", %lu%lu", sum_head, sum_tail);
+
+		a_head = b_head;
+		a_tail = b_tail;
+		b_head = sum_head;
+		b_tail = sum_tail;
+	}
+
+
+	printf("\n");
 
 	return (0);
 }
