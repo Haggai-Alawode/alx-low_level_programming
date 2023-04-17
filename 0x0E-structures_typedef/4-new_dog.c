acc10eeb7344b6bbdef11fdbce4f59638b121d8c
Haggai Alawode <keyboard@DESKTOP-9NVBJJL>
2023-04-17 18:19:33 +0100

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -43,13 +43,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 																							{
 																								free(cutie_dog->name);
 																								free(cutie_dog);
-																								return (NULL);
+					
+			return (NULL);
 																							}
-
+																							
 		cutie_dog->name = _strcpy(cutie_dog->name, name);
 																							cutie_dog->owner = _strcpy(cutie_dog->owner, owner);
 																							cutie_dog->age = age;
-																							}
+																							
 		return (cutie_dog);
 }
 
