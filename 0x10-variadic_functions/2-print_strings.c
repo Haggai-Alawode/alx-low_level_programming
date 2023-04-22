a2c3e9c78547df0072214ff22ade49fae12820df
Haggai Alawode <keyboard@DESKTOP-9NVBJJL>
2023-04-22 03:19:48 +0100

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -30,7 +30,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 																	{
 																		printf("%s", separator);
 																	}
-																}
+	
+	}
+
 	printf("\n");
 	va_end(args);
 }
